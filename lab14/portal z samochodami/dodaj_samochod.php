fe89beb078a69d7eeb5db8434be7818619939365
piotrkalinskipjatk <120741021+piotrkalinskipjatk@users.noreply.github.com>
2023-04-26 12:17:07 +0200

    Add files via upload

---
+++ lab14/portal z samochodami/dodaj_samochod.php
@@ -0,0 +1,63 @@
+<?php
+function checkVar()
+{
+    return isset($_POST['model']) && isset($_POST['marka']) && isset($_POST['rok']) && isset($_POST['cena']) && isset($_POST['opis']);
+}
+function checkNumeric()
+{
+    return is_numeric($_POST['rok']) && is_numeric($_POST['cena']) && $_POST['rok']>0 && $_POST['cena']>0;
+}
+
+if(checkVar()){
+    $marka = $_POST['marka'];
+    $model = $_POST['model'];
+    $rok = $_POST['rok'];
+    $cena = $_POST['cena'];
+    $opis = $_POST['opis'];
+}
+?>
+<FORM action="dodaj_samochod.php" method="post" >
+    <fieldset>
+        <TABLE>
+            <TR>
+                <TD>Marka:</TD>
+                <TD><INPUT name="marka" ></TD>
+            </TR>
+            <TR>
+                <TD>Model:</TD>
+                <TD><INPUT name="model" ></TD>
+            </TR>
+            <TR>
+                <TD>Rocznik:</TD>
+                <TD><INPUT name="rok" ></TD>
+            </TR>
+            <TR>
+                <TD>Cena:</TD>
+                <TD><INPUT name="cena" ></TD>
+            </TR>
+            <TR>
+                <TD>Opis:</TD>
+                <TD><INPUT name="opis" ></TD>
+            </TR>
+        </TABLE>
+    </fieldset>
+    <input type="submit" value="Wyslij">
+</FORM>
+<?php
+if(checkVar()){
+    if(checkNumeric()){
+        $db_connection = mysqli_connect("localhost", "root", "", "mojaBaza");
+        if (!$db_connection) {
+            echo "<h2>Błąd połączenia z bazą!</h2>";
+            exit();
+        } else {
+            $insert = "INSERT INTO samochody VALUES ('','$marka','$model','$cena','$rok','$opis')";
+            $result = mysqli_query($db_connection, $insert);
+            if (mysqli_affected_rows($db_connection) != 0) {
+                echo "Pojazd dodany pomyslnie";
+            } else echo "Pojazd nie zostal dodany";
+        }
+    } else echo "Brak lub niepoprawne dane";
+} else echo "Brak lub niepoprawne dane";
+?>
+<BR><a href="pierwsza_strona.php">Pierwsza strona
---
+++ lab14/portal z samochodami/pierwsza_strona.php
@@ -0,0 +1,11 @@
+
+<FORM action = "pierwsza_strona.php" method = "GET">
+    <fieldset>
+        <TABLE>
+            <TR><TD><a href="strona_glowna.php">Strona Glowna</TD></TR>
+            <TR><TD><a href="wszystkie_samochody.php">Wszystkie samochody</TD></TR>
+            <TR><TD><a href="dodaj_samochod.php">Dodaj samochod</TD></TR>
+        </TABLE>
+    </fieldset>
+</FORM>
+
